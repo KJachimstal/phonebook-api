4732b81f48b81531e4f1845f64e9f9b16f08a461
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-09-18 17:10:50 +0200

    Added apload image to route

--- api/routes/user-route.js
+++ api/routes/user-route.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const router = express.Router();
 const userController = require("../controllers/user-controller");
+const uploadImage = require("../config/multer");
 
 router.post("/signup", userController.signup);
 
@@ -10,6 +11,11 @@ router.post("/logout", userController.auth, userController.logout);
 
 router.get("/current", userController.auth, userController.current);
 
-router.patch("/avatars", userController.auth, userController.avatars);
+router.patch(
+  "/avatars",
+  userController.auth,
+  uploadImage.single("avatar"),
+  userController.avatars
+);
 
 module.exports = router;
