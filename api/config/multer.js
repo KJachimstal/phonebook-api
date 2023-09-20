c77e7d310eaef29c95f35da7fe43373d3dad8ad2
Konrad Jachimstal <k.jachimstal@gmail.com>
2023-09-20 10:57:31 +0200

    Changed randomize to user id avatar name

--- api/config/multer.js
+++ api/config/multer.js
@@ -2,16 +2,14 @@ const multer = require("multer");
 const path = require("path");
 const uploadDir = path.join(process.cwd(), "uploads");
 const storeImage = path.join(process.cwd(), "public/avatars");
-const uuid = require("uuid");
 
 const imageStorage = multer.diskStorage({
   destination: (req, file, cb) => {
     cb(null, uploadDir);
   },
   filename: (req, file, cb) => {
-    const randomFilename = uuid.v4();
-    const newFileName =
-      randomFilename + "." + file.originalname.split(".").pop();
+    const uIdFilename = req.user.id;
+    const newFileName = uIdFilename + "." + file.originalname.split(".").pop();
     cb(null, newFileName);
   },
   limits: {
